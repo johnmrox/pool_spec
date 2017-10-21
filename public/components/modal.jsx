d9de747635ac43268ab481a3ea39e374064217a4
johnmrox <johnmrox@gmail.com>
2017-10-21 08:00:13 -0400

    removed inline styles from modal

--- public/components/modal.jsx
+++ public/components/modal.jsx
@@ -3,31 +3,12 @@ var Modal = ({ onClose, isOpen, children }) => {
     return null;
   }
 
-  let modalStyle = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    zIndex: '9999',
-    background: '#fff'
-  };
-
-  let backdropStyle = {
-    position: 'fixed',
-    width: '100%',
-    height: '100%',
-    top: '0px',
-    left: '0px',
-    zIndex: '9998',
-    background: 'rgba(0, 0, 0, 0.3)'
-  };
-
   return (
     <div>
-      <div className="modal-overlay" style={modalStyle}>
+      <div className="modal-overlay">
         {children}
       </div>
-      <div className="modal-backdrop" style={backdropStyle} onClick={onClose} />
+      <div className="modal-backdrop" onClick={onClose} />
     </div>
   );
 };
